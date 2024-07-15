69f96bcdba26fb79339e8ac153b10ece075f01af
shoyama1010 <156488466+shoyama1010@users.noreply.github.com>
2024-07-15 15:56:54 +0900

    Add files via upload

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\ReservationController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +21,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 認証ルート
+Route::get('/register', [UserController::class, 'create'])->name('register');
+Route::post('/register', [UserController::class, 'store']);
+Route::get('/done', [UserController::class, 'done'])->name('done');
+
+Route::get('/login', [UserController::class, 'loginForm'])->name('login');
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+// ユーザールート
+// Route::get('/mypage', [UserController::class, 'mypage'])->middleware('auth')->name('mypage');
+Route::get('/mypage', [UserController::class, 'mypage'])->name('mypage');
+Route::get('/thanks', [UserController::class, 'thanks'])->name('thanks');
+
+// ショップ関連ルート
+Route::get('/', [ShopController::class, 'index'])->name('index');
+Route::get('/detail/{shop_id}', [ShopController::class, 'detail'])->name('detail');
+
+
+// お気に入り関連ルート
+Route::post('/favorite', [FavoriteController::class, 'store'])->name('favorite.store');
+Route::post('/favorite/{shop_id}', [FavoriteController::class, 'toggleFavorite'])->name('shop.toggleFavorite');
+// Route::delete('/favorite/{id}', [FavoriteController::class, 'destroy'])->name('favorite.destroy');
+Route::delete('/favorite/{shop_id}', [FavoriteController::class, 'toggleFavorite'])->name('shop.toggleFavorite');
+
+// 予約関連ルート
+Route::post('/reservation', [ReservationController::class, 'store'])->name('reservation.store');
+Route::delete('/reservation/{id}', [ReservationController::class, 'destroy'])->name('reservation.destroy');
